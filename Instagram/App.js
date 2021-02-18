dd1a4664da98ee6e1992eec54a6e8df7c1ba4237
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-18 11:57:14 -0300

    Clone do Instagram Basico
    Clone simples da Tela de feed do Instagram

---
+++ Instagram/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Index from "./src/Components/Index"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Index/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
